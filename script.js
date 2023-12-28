21e1ecf27557c50bf2edc10adf171836b250eb53
yosephwinata <yoseph.winata@gmail.com>
2023-12-28 19:40:04 +0700

    Fix popup

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
+// Get elements
 const featuresButton = document.querySelector("#features-dropdown");
 const featuresPopUp = document.querySelector("#features-popup");
 
-const handleFeaturesDropdownClick = () => {
+// Declare functions
+const handleFeaturesDropdownClick = (event) => {
+  console.log("toto");
   featuresPopUp.classList.toggle("hidden");
+  event.stopPropagation();
 };
 
+// Add event listeners
 featuresButton.addEventListener("click", handleFeaturesDropdownClick);
 
-// TODO: Use an invisible div beneath the pop up.
-// So when the user clicks outside the pop up, the pop up closes.
+// Hide popup when clicking outside of it
+document.addEventListener("click", function (event) {
+  if (!featuresPopUp.contains(event.target)) {
+    featuresPopUp.classList.add("hidden");
+  }
+});
+
+// Prevent the popup from closing when the user clicks on the popup
+featuresPopUp.addEventListener("click", (event) => {
+  event.stopPropagation();
+});
