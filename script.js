19431416b755418462d64e56e287a1972b30fca3
yosephwinata <yoseph.winata@gmail.com>
2023-12-29 12:55:11 +0700

    Complete dropdown popup

--- script.js
+++ script.js
@@ -1,25 +1,53 @@
 // Get elements
 const featuresButton = document.querySelector("#features-dropdown");
-const featuresPopUp = document.querySelector("#features-popup");
+const featuresText = document.querySelector("#features-text");
+const featuresArrow = document.querySelector("#features-arrow");
+const featuresPopup = document.querySelector("#features-popup");
 
-// Declare functions
-const handleFeaturesDropdownClick = (event) => {
-  console.log("toto");
-  featuresPopUp.classList.toggle("hidden");
+// Function to show popup
+function showPopup() {
+  featuresPopup.classList.remove("popup-hide");
+  featuresPopup.classList.add("popup-show");
+  featuresText.classList.add("black");
+}
+
+// Function to hide popup
+function hidePopup() {
+  featuresPopup.classList.add("popup-hide");
+  setTimeout(() => {
+    featuresPopup.classList.remove("popup-show");
+  }, 300); // Timeout matches the animation duration in the scss file
+  featuresText.classList.remove("black");
+}
+
+function toggleArrow() {
+  if (featuresPopup.classList.contains("popup-show")) {
+    featuresArrow.src = "/images/icon-arrow-down.svg";
+  } else {
+    featuresArrow.src = "/images/icon-arrow-up.svg";
+  }
+}
+
+// Toggle popup on navItem click
+featuresButton.addEventListener("click", function (event) {
   event.stopPropagation();
-};
-
-// Add event listeners
-featuresButton.addEventListener("click", handleFeaturesDropdownClick);
-
-// Hide popup when clicking outside of it
-document.addEventListener("click", function (event) {
-  if (!featuresPopUp.contains(event.target)) {
-    featuresPopUp.classList.add("hidden");
+  toggleArrow();
+  if (featuresPopup.classList.contains("popup-show")) {
+    hidePopup();
+  } else {
+    showPopup();
   }
 });
 
-// Prevent the popup from closing when the user clicks on the popup
-featuresPopUp.addEventListener("click", (event) => {
+// Hide popup when clicking outside
+document.addEventListener("click", function () {
+  if (featuresPopup.classList.contains("popup-show")) {
+    toggleArrow();
+    hidePopup();
+  }
+});
+
+// Prevent the popup from closing if the user clicks on the popup
+featuresPopup.addEventListener("click", (event) => {
   event.stopPropagation();
 });
