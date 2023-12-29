32b1c7e8e4c2fcdd3d69d7f27250e41c3b9507cc
yosephwinata <yoseph.winata@gmail.com>
2023-12-29 16:18:05 +0700

    Finish desktop version

--- script.js
+++ script.js
@@ -1,100 +1,80 @@
 // Get elements
 const navItems = document.querySelectorAll(".navbar__dropdown");
-// const navItemText = document.querySelector("#features-text");
-// const navItemArrow = document.querySelector("#features-arrow");
 const popups = document.querySelectorAll(".navbar-popup");
+const texts = document.querySelectorAll(".navbar__text");
+const arrows = document.querySelectorAll(".navbar__arrow-icon");
 
-// // Function to show popup
-// function showPopup() {
-//   featuresPopup.classList.remove("popup-hide");
-//   featuresPopup.classList.add("popup-show");
-//   navItemText.classList.add("black");
-// }
-
-// Function to show a specific popup
+// Show a specific popup
 function showPopup(popup) {
   popup.classList.remove("popup-hide");
   popup.classList.add("popup-show");
 }
 
-// // Function to hide popup
-// function hidePopup() {
-//   featuresPopup.classList.add("popup-hide");
-//   setTimeout(() => {
-//     featuresPopup.classList.remove("popup-show");
-//   }, 300); // Timeout matches the animation duration in the scss file
-//   navItemText.classList.remove("black");
-// }
-
-// Function to hide a specific popup
+// Hide a specific popup
 function hidePopup(popup) {
   popup.classList.add("popup-hide");
   popup.classList.remove("popup-show");
 }
 
-function toggleArrow(popup) {
-  if (featuresPopup.classList.contains("popup-show")) {
-    navItemArrow.src = "/images/icon-arrow-down.svg";
-  } else {
-    navItemArrow.src = "/images/icon-arrow-up.svg";
-  }
+// Add highlight to a specific text
+function addTextHighlight(text) {
+  text.classList.add("black");
 }
 
-// Function to toggle popup
-function togglePopup(popup) {
-  if (popup.classList.contains("popup-show")) {
-    hidePopup(popup);
-  } else {
-    showPopup(popup);
-  }
+// Remove highlight from a specific text
+function removeTextHighlight(text) {
+  text.classList.remove("black");
 }
 
-// // Toggle popup on navItem click
-// featuresButton.addEventListener("click", function (event) {
-//   event.stopPropagation();
-//   toggleArrow();
-//   if (featuresPopup.classList.contains("popup-show")) {
-//     hidePopup();
-//   } else {
-//     showPopup();
-//   }
-// });
+// Change the arrow image to the one that points upwards
+function setUpArrow(arrow) {
+  arrow.src = "/images/icon-arrow-up.svg";
+}
+
+// Change the arrow image to the one that points downwards
+function setDownArrow(arrow) {
+  arrow.src = "/images/icon-arrow-down.svg";
+}
 
 // Attach event listeners to each nav item
 navItems.forEach((navItem) => {
   navItem.addEventListener("click", function (event) {
-    // event.preventDefault();
     event.stopPropagation();
 
-    const popupId = navItem.getAttribute("data-popup");
-    const popup = document.getElementById(popupId);
-
-    // Hide all popups
+    // Reset all states
     popups.forEach((p) => hidePopup(p));
+    texts.forEach((t) => removeTextHighlight(t));
+    arrows.forEach((a) => setDownArrow(a));
 
-    // Toggle the clicked nav item's popup
-    togglePopup(popup);
+    const popupId = navItem.getAttribute("data-popup");
+    const textId = navItem.getAttribute("data-text");
+    const arrowId = navItem.getAttribute("data-arrow");
+
+    const popup = document.getElementById(popupId);
+    const text = document.getElementById(textId);
+    const arrow = document.getElementById(arrowId);
+
+    showPopup(popup);
+    addTextHighlight(text);
+    setUpArrow(arrow);
   });
 });
 
-// // Hide popup when clicking outside
-// document.addEventListener("click", function () {
-//   if (featuresPopup.classList.contains("popup-show")) {
-//     toggleArrow();
-//     hidePopup();
-//   }
-// });
-
-// Hide all popups when clicking outside
+// When clicking outside the popup, hide all popouts, reset all arrows and all text colors
 document.addEventListener("click", function () {
-  popups.forEach((popup) => {
+  navItems.forEach((navItem) => {
+    const popupId = navItem.getAttribute("data-popup");
+    const textId = navItem.getAttribute("data-text");
+    const arrowId = navItem.getAttribute("data-arrow");
+
+    const popup = document.getElementById(popupId);
+    const text = document.getElementById(textId);
+    const arrow = document.getElementById(arrowId);
+
     if (popup.classList.contains("popup-show")) {
       hidePopup(popup);
+      removeTextHighlight(text);
+      setDownArrow(arrow);
     }
   });
 });
-
-// Prevent the popup from closing if the user clicks on the popup
-// featuresPopup.addEventListener("click", (event) => {
-//   event.stopPropagation();
-// });
